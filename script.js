602d911eaf80752a77a667f509e768f79936ec40
randrianiaina <63423231+randrianiaina@users.noreply.github.com>
2023-03-07 16:18:31 +0300

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello!');
+const listTodos = [];
+
+// Define the showPrompt function
+function showPrompt() {
+  let todo = prompt('Saisir la tache :');
+  if (todo !== null) {
+    console.log(todo);
+  }
+}
+// const inputEvent = (e)=>{
+//   console.log(e.target.value)
+// }
+const showResult = () => {
+  //result.innerText = JSON.stringify(listTodos);
+  const div = document.createElement('div');
+  div.innerText=
+};
